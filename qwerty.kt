9986c2b1a7b377380655c49ec3b1df9f3b2e17f3
Vlada Pogozhelskaya <pogozhelskaya@gmail.com>
2019-03-04 17:56:48 +0300

    Added Ford Bellman

--- qwerty.kt
+++ qwerty.kt
@@ -1,5 +1,5 @@
 fun build(N: Int, M: Int) : Array<IntArray> {
-    val adjacencyMatrix = Array(N, { IntArray(N) })
+    val adjacencyMatrix = Array(N) { IntArray(N) }
     val edges : HashSet<Pair<Int, Int>> = HashSet()
     while (edges.size != M) {
         var begin = (1..N).random()
@@ -33,36 +33,72 @@ fun statistic(adjacencyMatrix : Array<IntArray>, N : Int, M: Int) {
     println("The lightest edge is $minEdge and the heaviest is $maxEdge")
 }
 
-fun dijkstra(adjacencyMatrix: Array<IntArray>, N: Int, K: Int) {
-    val lengths : Array<Int> = Array(N, { Int.MAX_VALUE })
-    val used : Array<Boolean> = Array(N, { false })
-    lengths[K] = 0
+fun dijkstra(adjacencyMatrix: Array<IntArray>, N: Int, K: Int) : Array<Int> {
+    val distances: Array<Int> = Array(N) { Int.MAX_VALUE }
+    val used : Array<Boolean> = Array(N) { false }
+    distances[K] = 0
     for (i in 0..(N - 1)) {
         var v = -1
         for (j in 0..(N - 1)) {
-            if (used[j] == false && (v == -1 || lengths[j] < lengths[v])) {
+            if (used[j] == false && (v == -1 || distances[j] < distances[v])) {
                 v = j
             }
         }
-        if (lengths[v] == Int.MAX_VALUE) break
+        if (distances[v] == Int.MAX_VALUE) break
         used[v] = true
         for (j in 0..(N - 1)) {
             if (adjacencyMatrix[v][j] == 0) continue
-            if (lengths[v] + adjacencyMatrix[v][j] < lengths[j]) {
-                lengths[j] = lengths[v] + adjacencyMatrix[v][j]
+            if (distances[v] + adjacencyMatrix[v][j] < distances[j]) {
+                distances[j] = distances[v] + adjacencyMatrix[v][j]
             }
         }
     }
     for(i in 0..N - 1) {
-        if(lengths[i] == Int.MAX_VALUE)
-            println("There's no way from vertex number $K to vertex number ${i + 1}")
+        if(distances[i] == Int.MAX_VALUE)
+            println("There's no way from vertex number ${K + 1} to vertex number ${i + 1}")
         else
-            println("From $K to ${i + 1}: ${lengths[i]}")
+            println("From ${K + 1} to ${i + 1}: ${distances[i]}")
     }
+    return distances
+}
+
+fun fordBellman(adjacencyMatrix: Array<IntArray>, N: Int, K: Int) : Array<Int> {
+    data class Edge(val from: Int, val to: Int, val weight: Int)
+    var edges: Array<Edge> = emptyArray()
+    for(i in 0..(N - 1)) {
+        for (j in i..(N - 1)) {
+           // val edge = Edge(i, j, adjacencyMatrix[i][j])
+            edges = edges.plus(Edge(i, j, adjacencyMatrix[i][j]))
+            edges = edges.plus(Edge(j, i, adjacencyMatrix[i][j]))
+        }
+    }
+    val distances : Array<Int> = Array(N, { Int.MAX_VALUE })
+    distances[K] = 0
+    while(true) {
+        var any = false
+        for (edge in edges) {
+            if(distances[edge.from] < Int.MAX_VALUE) {
+                if (distances[edge.to] > distances[edge.from] + edge.weight) {
+                    distances[edge.to] = distances[edge.from] + edge.weight
+                    any = true
+                }
+            }
+        }
+        if(!any) break
+    }
+    for(i in 0..N - 1) {
+        if(distances[i] == Int.MAX_VALUE)
+            println("There's no way from vertex number ${K + 1} to vertex number ${i + 1}")
+        else
+            println("From ${K + 1} to ${i + 1}: ${distances[i]}")
+    }
+    return distances
 }
 
 fun main(args : Array<String>) {
     val (N, M) = readLine()!!.split(' ').map(String::toInt)
+    val fordBellmanArray : Array<Int>
+    val dijkstraArray : Array<Int>
     val adjacencyMatrix : Array<IntArray> = build(N, M)
     for (row in adjacencyMatrix) {
         for (element in row) {
@@ -74,5 +110,16 @@ fun main(args : Array<String>) {
     statistic(adjacencyMatrix, N, M)
     println("Please, enter K:")
     val K = readLine()!!.toInt()
-    dijkstra(adjacencyMatrix, N, K - 1)
+    dijkstraArray = dijkstra(adjacencyMatrix, N, K - 1)
+    fordBellmanArray = fordBellman(adjacencyMatrix, N, K - 1)
+    if (dijkstraArray.size != fordBellmanArray.size) {
+        println("Algorithms are incorrect 1")
+    } else {
+        for (i in 0..(dijkstraArray.size - 1)) {
+            if (dijkstraArray[i] != fordBellmanArray[i]) {
+                println("Algorithms are incorrect 2")
+                break
+            }
+        }
+    }
 }
